28f8168d605b6c8be7fcb9704291b85da65ef48e
Nickolay_Kryzhanovskiy <kryzhanovskiy96@gmail.com>
2022-06-06 12:50:29 +0300

    monday

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+    autoplay: {
+        delay: 5000,
+      },
+      navigation: {
+        nextEl: '.swiper-next',
+        prevEl: '.swiper-prev',
+      },
+})
